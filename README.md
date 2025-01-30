74045868ac8834e1fe846f3773e7b5d2fcb31e5a
Pedro Goncalves Mokarzel <pgmoka@google.com>
2025-01-30 01:54:44 +0000

    Add initial dataset parsing

---
+++ README.md
@@ -0,0 +1,3 @@
+Repo goes through
+https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial for
+PyTorch, and then replicates it in PyTorch XLA.
---
+++ pytorch_example/rnn.py
@@ -0,0 +1,64 @@
+from io import open
+import glob
+import os
+import time
+
+import torch
+from torch.utils.data import Dataset
+
+print('-- Initiate model declaration')
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return allowed_characters.find(letter)
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+class NamesDataset(Dataset):
+
+    def __init__(self, data_dir):
+        self.data_dir = data_dir #for provenance of the dataset
+        self.load_time = time.localtime #for provenance of the dataset
+        labels_set = set() #set of all classes
+
+        self.data = []
+        self.data_tensors = []
+        self.labels = []
+        self.labels_tensors = []
+
+        #read all the ``.txt`` files in the specified directory
+        text_files = glob.glob(os.path.join(data_dir, '*.txt'))
+        for filename in text_files:
+            label = os.path.splitext(os.path.basename(filename))[0]
+            labels_set.add(label)
+            lines = open(filename, encoding='utf-8').read().strip().split('\n')
+            for name in lines:
+                self.data.append(name)
+                self.data_tensors.append(lineToTensor(name))
+                self.labels.append(label)
+
+        #Cache the tensor representation of the labels
+        self.labels_uniq = list(labels_set)
+        for idx in range(len(self.labels)):
+            temp_tensor = torch.tensor([self.labels_uniq.index(self.labels[idx])], dtype=torch.long)
+            self.labels_tensors.append(temp_tensor)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        data_item = self.data[idx]
+        data_label = self.labels[idx]
+        data_tensor = self.data_tensors[idx]
+        label_tensor = self.labels_tensors[idx]
+
+        return label_tensor, data_tensor, data_label, data_item
+
+print('-- Model compiled')
+
