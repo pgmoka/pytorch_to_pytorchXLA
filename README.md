9be146ed1cec89c1898e6e61f32b5762aa8b494b
Pedro Goncalves Mokarzel <pgmoka@google.com>
2025-02-04 19:31:15 +0000

    Initial changes to PyTorh XLA case

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Repo goes through
 https://pytorch.org/tutorials/intermediate/char_rnn_classification_tutorial for
 PyTorch, and then replicates it in PyTorch XLA.
+
+Data pulled from `https://download.pytorch.org/tutorial/data.zip`
--- pytorchxla_example/rnn.py
+++ pytorchxla_example/rnn.py
@@ -1,3 +1,14 @@
+import torch_xla as xla
+import torch_xla.core.xla_model as xm
+import torch
+import torch.nn as nn
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import random
+import time
+import numpy as np
+
 print('-- Initiate model and training declaration')
 
 class CharRNN(nn.Module):
@@ -28,35 +39,39 @@ def train(rnn, training_data, n_epoch = 10, n_batch_size = 64, report_every = 50
     start = time.time()
     print(f"training on data set with n = {len(training_data)}")
 
+    rnn.to(xla.device())
+
     for iter in range(1, n_epoch + 1):
-        rnn.zero_grad() # clear the gradients
+        with xla.step():
+          rnn.zero_grad() # clear the gradients
 
-        # create some minibatches
-        # we cannot use dataloaders because each of our names is a different length
-        batches = list(range(len(training_data)))
-        random.shuffle(batches)
-        batches = np.array_split(batches, len(batches) //n_batch_size )
+          # create some minibatches
+          # we cannot use dataloaders because each of our names is a different length
+          batches = list(range(len(training_data)))
+          random.shuffle(batches)
+          batches = np.array_split(batches, len(batches) //n_batch_size )
 
-        for idx, batch in enumerate(batches):
-            batch_loss = 0
-            for i in batch: #for each example in this batch
-                (label_tensor, text_tensor, label, text) = training_data[i]
-                output = rnn.forward(text_tensor)
-                loss = criterion(output, label_tensor)
-                batch_loss += loss
+          for idx, batch in enumerate(batches):
+              idx, batch = idx.to(xla.device()), batch.to(xla.device())
+              batch_loss = 0
+              for i in batch: #for each example in this batch
+                  (label_tensor, text_tensor, label, text) = training_data[i]
+                  output = rnn.forward(text_tensor)
+                  loss = criterion(output, label_tensor)
+                  batch_loss += loss
 
-            # optimize parameters
-            batch_loss.backward()
-            nn.utils.clip_grad_norm_(rnn.parameters(), 3)
-            optimizer.step()
-            optimizer.zero_grad()
+              # optimize parameters
+              batch_loss.backward()
+              nn.utils.clip_grad_norm_(rnn.parameters(), 3)
+              xm.optimizer_step(optimizer)
+              optimizer.zero_grad()
 
-            current_loss += batch_loss.item() / len(batch)
+              current_loss += batch_loss.item() / len(batch)
 
-        all_losses.append(current_loss / len(batches) )
-        if iter % report_every == 0:
-            print(f"{iter} ({iter / n_epoch:.0%}): \t average batch loss = {all_losses[-1]}")
-        current_loss = 0
+          all_losses.append(current_loss / len(batches) )
+          if iter % report_every == 0:
+              print(f"{iter} ({iter / n_epoch:.0%}): \t average batch loss = {all_losses[-1]}")
+          current_loss = 0
 
     return all_losses
 
